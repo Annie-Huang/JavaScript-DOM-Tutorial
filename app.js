a6df93b481c3e85e88afc365deb37f0633d5dee0
Annie Huang <annie.hn@gmail.com>
2019-05-23 22:00:57 +1000

    Lesson 2: Get Element By ID

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log(document.getElementById('page-banner'));
+var banner = document.getElementById('page-banner');
+console.log(banner);
+
+var bookList = document.getElementById('book-list');
+console.log(bookList);
+
+console.log(document);
