af49297f324f5ec54b6b8cb85d774f32e62168e3
Annie Huang <annie.hn@gmail.com>
2019-05-25 10:22:23 +1000

    #6 - Nodes

--- app.js
+++ app.js
@@ -76,7 +76,16 @@
 // // bookList.innerHTML = '<h2>Books and more books...</h2>';
 // bookList.innerHTML += '<p>This is how you add HTML</p>';
 
+// #6 - Nodes
+const banner = document.querySelector('#page-banner');
+// https://www.w3schools.com/jsref/prop_node_nodetype.asp
+console.log('#page-banner node type is:', banner.nodeType); // return 1, which is Element
+console.log('#page-banner node name is:', banner.nodeName); // return div
+console.log('#page-banner has child nodes is:', banner.hasChildNodes()); // return div
 
+const clonedBanner = banner.cloneNode(false);
+// const clonedBanner = banner.cloneNode(true); // true means clone its children as well.
+console.log(clonedBanner);
 
 console.log(document);
 
