69bc56b0542efde452b81879cae1e2fd55660032
Annie Huang <annie.hn@gmail.com>
2019-05-25 12:13:37 +1000

    #9 - Events

--- app.js
+++ app.js
@@ -111,6 +111,32 @@
 //
 // bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else!';
 
+// // #9 - Events
+//
+// // var h2 = document.querySelector('#book-list h2');
+// // console.log(h2);
+// // // https://www.w3schools.com/jsref/dom_obj_event.asp
+// // h2.addEventListener('click', function (e) {
+// //     console.log(e.target);  // log the element
+// //     // log MouseEvent, it got clientX, clientY (X,Y position on the click), shiftKey (whether you press shift key when you make the click)
+// //     // many interesting things, take a look
+// //     console.log(e);
+// // });
+//
+// var btns = document.querySelectorAll('#book-list .delete');
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         const li = e.target.parentElement;
+//         li.parentNode.removeChild(li);
+//     })
+// });
+//
+// const link = document.querySelector('#page-banner a');
+// link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     console.log('navigation to', e.target.textContent, 'was prevented');
+// });
+
 console.log(document);
 
 
