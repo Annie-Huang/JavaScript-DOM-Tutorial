507b78e69426c7f2c351ccef4a3629097ed9ed51
Annie Huang <annie.hn@gmail.com>
2019-05-25 21:28:00 +1000

    #16 - Custom Search Filter

--- app.js
+++ app.js
@@ -294,6 +294,28 @@
 //     }
 // });
 
+// // #16 - Custom Search Filter
+// // search usually links to key up event
+//
+// const list = document.querySelector('#book-list ul');
+//
+// // filter books
+// const searchBar = document.forms['search-books'].querySelector('input[type="text"]');
+// // console.log(searchBar);
+// searchBar.addEventListener('keyup', function (e) {
+//     const term = e.target.value.toLowerCase();
+//     // console.log(term);
+//     const books = list.getElementsByTagName('li');
+//     Array.from(books).forEach(function (book) {
+//         const title = book.firstElementChild.textContent;
+//         if(title.toLowerCase().indexOf(term) != -1) {
+//             book.style.display = 'block';
+//         } else {
+//             book.style.display = 'none';
+//         }
+//     })
+// });
+
 console.log(document);
 
 
